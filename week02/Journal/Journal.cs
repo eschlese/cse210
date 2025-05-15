0c5acb8befa74ce5ad6b434f720fe6456cf06e74
salsadebil <188414316+salsadebil@users.noreply.github.com>
2025-05-14 22:02:25 -0700

    started week02 journal program

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks.Dataflow;
+
+public class Journal
+{
+    public List<Entry> _entryList = new List<Entry>();
+
+    public void Write(Entry newEntry)
+    {
+
+    }
+
+    public void DisplayAll()
+    {
+
+    }
+
+    public void Save(string file)
+    {
+
+    }
+
+    public void Load(string file)
+    {
+        
+    }
+}
