0377ce31594d55249f4b52e462de9cfa19df2d08
salsadebil <188414316+salsadebil@users.noreply.github.com>
2025-05-14 20:46:45 -0700

    completed week02 learning activity

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,5 +5,23 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Job job1 = new Job();
+        job1._company = "Arcteryx";
+        job1._jobTitle = "Web Developer";
+        job1._startYear = "2008";
+        job1._endYear = "2012";
+
+        Job job2 = new Job();
+        job2._company = "Bioware";
+        job2._jobTitle = "Software Developer";
+        job2._startYear = "2012";
+        job2._endYear = "2013";
+
+        Resume resume = new Resume();
+        resume._name = "Mike Wazowski";
+        resume._jobs.AddRange([job1, job2]);
+
+        resume.Display();
     }
 }
