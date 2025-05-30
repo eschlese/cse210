256f416a900e3459650d395908e8569517a495e5
eschlese <188414316+eschlese@users.noreply.github.com>
2025-05-29 20:03:23 -0700

    finished fraction activity and week4 designs

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction fractionOne = new Fraction();
+        Fraction fractionTwo = new Fraction(6);
+        Fraction fractionThree = new Fraction(6, 7);
+
+        Console.WriteLine(fractionOne.GetFractionString());
+        Console.WriteLine(fractionTwo.GetFractionString());
+        Console.WriteLine(fractionThree.GetFractionString());
+
+        fractionOne.SetTop(8);
+        fractionOne.SetBottom(9);
+        Console.WriteLine(fractionOne.GetTop());
+        Console.WriteLine(fractionOne.GetBottom());
+
+        Console.WriteLine(fractionOne.GetDecimalValue());
+        Console.WriteLine(fractionTwo.GetDecimalValue());
+        Console.WriteLine(fractionThree.GetDecimalValue());
     }
 }
